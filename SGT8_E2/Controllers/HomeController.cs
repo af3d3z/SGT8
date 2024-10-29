22ffd9610781a757a5933ad0caebf2b20bb4a61c
th3-m0th <12004683-th3-m0th@users.noreply.gitlab.com>
2024-10-29 20:48:45 +0100

    Initial commit

---
+++ SGT8_E2/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using SGT8_E2.Models;
+using System.Diagnostics;
+
+namespace SGT8_E2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string nombre) {
+            ViewBag.nombre = nombre;
+            return View("Saludo");
+        }
+
+        public IActionResult Saludo() {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SGT8_E2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SGT8_E2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
